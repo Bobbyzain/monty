5107e9bb4a9f721f1dac391fb7529d65017d13c0
wittyj <wittyjessy2002@yahoo.com>
2023-11-19 16:06:05 -0800

    pushing our opcodes getter

---
+++ get_opcodesx.c
@@ -0,0 +1,41 @@
+#include "monty.h"
+
+/**
+ * get_opcodesx - selects the correct opcode to perform
+ *
+ * @opcd: opcode passed
+ *
+ * Return: pointer to the function that executes the opcode
+ */
+void (*get_opcodesx(char *opcd))(stack_t **stack, unsigned int line_number)
+{
+	instruction_t instruct[] = {
+		{"push", _push_L},
+		{"pall", _pall_L},
+		{"pint", _pint_L},
+		{"pop", _pops},
+		{"swap", _swap_L},
+		{"queue", _queuer},
+		{"stack", _stackr},
+		{"add", _adds},
+		{"nop", _nops},
+		{"sub", _subs},
+		{"mul", _mul},
+		{"div", _divs},
+		{"mod", _mods},
+		{"pchar", _pchars},
+		{"pstr", _pstrs},
+		{"rotl", _rots_L},
+		{"rotr", _rot_rv},
+		{NULL, NULL}
+	};
+	int i;
+
+	for (i = 0; instruct[i].opcode; i++)
+	{
+		if (_strcmp(instruct[i].opcode, opcd) == 0)
+			break;
+	}
+
+	return (instruct[i].f);
+}
